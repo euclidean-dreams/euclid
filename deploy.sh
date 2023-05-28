38ae77559692bf7f78a4f67e1c8f09aae9a30f2f
josiah <josiah.chapman@gmail.com>
2023-05-27 17:57:52 -0700

    beam

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ read tag
 
 # build image
 cd deploy/proscenium &&
-docker buildx build --platform linux/amd64 -t josiahdc/proscenium:$tag . --push &&
+docker buildx build --platform linux/amd64 -t josiahdc/proscenium:${tag} . --push &&
 
 # deploy image to gcp
-gcloud run deploy proscenium-$tag --image josiahdc/proscenium:$tag --region us-west2
+gcloud run deploy proscenium-${tag} --image josiahdc/proscenium:${tag} --region us-west2
