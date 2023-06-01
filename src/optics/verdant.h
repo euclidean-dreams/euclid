15398590aa0b81d05baaed6b4cec73922d96a3ad
josiah <josiah.chapman@gmail.com>
2023-05-31 21:06:13 -0700

    (~)

--- src/optics/verdant.h
+++ src/optics/verdant.h
@@ -32,6 +32,7 @@ public:
     float rotation;
     float energy_requirement;
     lst<Point> body;
+    int age = 0;
 
     Foliage(Luon &luon, Point origin, float rotation, float energy_requirement) :
             luon{luon},
@@ -48,7 +49,9 @@ public:
         }
     }
 
-    bool keep_alive() {
+    bool live() {
+        age += 1;
+        // return true to continue being alive
         if (luon.energy >= energy_requirement)
             return true;
         return false;
@@ -69,7 +72,7 @@ public:
         auto blue = 255 - trim(color_mag * 33, 0, 255);
 
         body.clear();
-        int scale = luon.energy / 10;
+        int scale = luon.energy / 23;
         for (int x = origin.x; x < origin.x + scale; x++) {
             for (int y = origin.y; y < origin.y + scale; y++) {
                 body.emplace_back(x, y);
@@ -120,7 +123,7 @@ public:
 
         auto cur_leaf = leaves.begin();
         while (cur_leaf != leaves.end()) {
-            if (not cur_leaf->keep_alive())
+            if (not cur_leaf->live())
                 cur_leaf = leaves.erase(cur_leaf);
             else
                 cur_leaf++;
@@ -142,50 +145,42 @@ public:
     }
 
     void draw(Canvas &canvas) {
-        if (luon.energy > energy_requirement) {
-            int displacement = abs(luon.delta * 2);
-            while (displacement > 0) {
-                auto step_distance = 0.5;
-                displacement -= step_distance;
-                if (luon.delta > 0) {
-                    auto cur_tip = *(--body.end());
-                    auto new_position = Point::from_polar(step_distance, rotation);
-                    body.emplace_back(cur_tip.x + new_position.x, cur_tip.y + new_position.y);
-                } else {
-                    if (not body.empty())
-                        body.pop_back();
-                }
+        int displacement = abs(luon.delta * 2);
+        while (displacement > 0) {
+            auto step_distance = 0.5;
+            displacement -= step_distance;
+            if (luon.delta > 0) {
+                auto cur_tip = *(--body.end());
+                auto new_position = Point::from_polar(step_distance, rotation);
+                body.emplace_back(cur_tip.x + new_position.x, cur_tip.y + new_position.y);
+            } else {
+                if (not body.empty())
+                    body.pop_back();
             }
+        }
 
-            // color - luon energy
-            auto color_mag = luon.energy * 10;
-            auto red = 255 - trim(color_mag / 33, 0, 255);
-            auto green = 255 - trim(color_mag / 3, 0, 255);
-            auto blue = 255 - trim(color_mag * 33, 0, 255);
+        // color - luon energy
+        auto color_mag = luon.energy * 10;
+        auto red = 255 - trim(color_mag / 33, 0, 255);
+        auto green = 255 - trim(color_mag / 3, 0, 255);
+        auto blue = 255 - trim(color_mag * 33, 0, 255);
 
-            paint_body(canvas, {red, green, blue});
+        paint_body(canvas, {red, green, blue});
 
-            // trough detected
-            if (luon.prior_delta <= 0 && luon.delta > 0) {
-                branches.emplace_back(luon, *(--body.end()), cos(rand() % 64 / 16) * M_PI / 3, luon.energy);
-            }
+        // trough detected
+        if (luon.prior_delta <= 0 && luon.delta > 0) {
+            branches.emplace_back(luon, *(--body.end()), cos(age / 16) * M_PI / 3, luon.energy);
+        }
 
-            auto cur_branch = branches.begin();
-            while (cur_branch != branches.end()) {
-                if (not cur_branch->keep_alive())
-                    cur_branch = branches.erase(cur_branch);
-                else
-                    cur_branch++;
-            }
-            for (auto &branch: branches) {
-                branch.show(canvas);
-            }
-
-        } else {
-            branches.clear();
-            body.clear();
-            origin = {scast<float>(rand() % canvas.width), scast<float>(rand() % canvas.height)};
-            body.emplace_back(origin);
+        auto cur_branch = branches.begin();
+        while (cur_branch != branches.end()) {
+            if (not cur_branch->live())
+                cur_branch = branches.erase(cur_branch);
+            else
+                cur_branch++;
+        }
+        for (auto &branch: branches) {
+            branch.show(canvas);
         }
     }
 };
@@ -196,25 +191,36 @@ private:
     int height;
     sp<Psyche> psyche;
     lst<Stalk> stalks;
+    int age;
 
 public:
     Verdant(int width, int height, sp<Psyche> psyche)
             : width{width},
               height{height},
               psyche{psyche} {
-        for (auto &luon: *psyche->luons) {
-            Point origin{scast<float>(rand() % width), scast<float>(rand() % height)};
-            stalks.emplace_back(luon, origin, scast<float>(2 * M_PI * cos(luon.fundamental / 64)), 3);
-        }
+
     }
 
     SDL_Texture *observe() {
+        age++;
         auto pixels = mkup<Signal<Pixel>>();
         pixels->populate(width * height, Pixel{255, 255, 255});
         int pitch = scast<int>(width * sizeof(Pixel));
         //////////////////////////////////////////////////////////////
-
         Canvas canvas{width, height, *pixels};
+
+        for (auto &luon: *psyche->luons) {
+            Point origin{scast<float>(rand() % width), scast<float>(rand() % height)};
+            stalks.emplace_back(luon, origin, scast<float>(2 * M_PI * cos(age / 64)), 3);
+        }
+
+        auto cur_stalk = stalks.begin();
+        while (cur_stalk != stalks.end()) {
+            if (not cur_stalk->live())
+                cur_stalk = stalks.erase(cur_stalk);
+            else
+                cur_stalk++;
+        }
         for (auto &stalk: stalks) {
             stalk.draw(canvas);
         }
